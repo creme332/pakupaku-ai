080689dd4fd22a2c3a57c42d1ab70bb78dfda06d
creme332 <65414576+creme332@users.noreply.github.com>
2024-01-06 14:06:16 +0400

    define a game interface

--- docs/pakupaku/bot.js
+++ docs/pakupaku/bot.js
@@ -1,29 +1,168 @@
-(function botController() {
-  function play() {
-    const playerSpeed = 0.5 * difficulty;
+/**
+ * A game interface. Use this interface to get/set information in your bot algorithm.
+ * Do not modify this code unless you know what you are doing.
+ * @returns
+ */
+function interface() {
+  let previousPlayerPosition;
 
-    if (!player || !enemy) return;
+  /**
+   * Saves current position of player. This function is
+   * used to determine if game is ongoing.
+   *
+   * NOTE: Do not call this function inside of your strategy.
+   */
+  function savePlayerPosition() {
+    previousPlayerPosition = getPlayerPosition();
+  }
 
-    // console.log(player);
-    // console.log(animTicks);
-    console.log(enemy);
-    console.log(playerSpeed);
+  /**
+   * Returns velocity of enemy
+   * @returns
+   */
+  function getEnemyVelocity() {
+    if (!enemy) return;
+    const evx =
+      enemy.eyeVx !== 0
+        ? enemy.eyeVx
+        : (player.x > enemy.x ? 1 : -1) * (powerTicks > 0 ? -1 : 1);
+    const enemyVelocity =
+      evx *
+      (powerTicks > 0 ? 0.25 : enemy.eyeVx !== 0 ? 0.75 : 0.55) *
+      difficulty;
 
-    // reverseDirection();
+    return enemyVelocity;
+  }
+
+  /**
+   * Check if game is ongoing. Game is said to be ongoing if
+   * player is moving.
+   * @returns
+   */
+  function gameOngoing() {
+    if (!player || !enemy) return false;
+
+    const currentPlayerPosition = Math.floor(getPlayerPosition());
+    previousPlayerPosition = Math.floor(previousPlayerPosition);
+
+    // check if player is moving
+    return currentPlayerPosition !== previousPlayerPosition;
+  }
+
+  /**
+   * Returns velocity of player
+   * @returns float
+   */
+  function getPlayerVelocity() {
+    if (!player) return;
+    return 0.5 * difficulty * getPlayerDirection();
+  }
+
+  /**
+   * Position of player on x-axis
+   * @returns A value 0-100
+   */
+  function getPlayerPosition() {
+    return player?.x;
+  }
+
+  /**
+   * Position of player on x-axis
+   * @returns A value 1-100
+   */
+  function getEnemyPosition() {
+    return enemy?.x;
+  }
+
+  /**
+   * Movement direction of player
+   * @returns 1 if player is moving right and -1 otherwise
+   */
+  function getPlayerDirection() {
+    return player?.vx;
+  }
+
+  /**
+   * Movement direction of enemy
+   * @returns 1 if enemy is moving right and -1 otherwise
+   */
+  function getEnemyDirection() {
+    if (!gameOngoing) return;
+    return getEnemyVelocity() > 0 ? 1 : -1;
   }
 
   /**
    * Reverse player movement direction.
    * @returns void
    */
-  function reverseDirection() {
-    if (!player) {
-      return;
-    }
-    console.log("Changed direction");
+  function reversePlayerDirection() {
+    if (!player) return;
     player.vx *= -1;
   }
 
-  // make a move every 1s
-  setInterval(play, 1000);
+  /**
+   * Get player attributes
+   * @returns
+   */
+  function getPlayer() {
+    return {
+      position: getPlayerPosition(),
+      velocity: getPlayerVelocity(),
+    };
+  }
+
+  /**
+   *
+   * @returns An array of objects about dots. Each object
+   * contains dot position and whether or not dot is a Power.
+   */
+  function getDots() {
+    return [...dots];
+  }
+
+  /**
+   * Get enemy attributes
+   * @returns
+   */
+  function getEnemy() {
+    return {
+      position: getEnemyPosition(),
+      velocity: getEnemyVelocity(),
+    };
+  }
+
+  return {
+    getPlayer,
+    gameOngoing,
+    getEnemy,
+    reversePlayerDirection,
+    savePlayerPosition,
+    getDots,
+  };
+}
+
+(function botController() {
+  const game = interface();
+  setInterval(main, 1000);
+
+  /**
+   * Driver function for bot.
+   * Do not modify this code unless you know what you are doing.
+   * @returns void
+   */
+  function main() {
+    if (!game.gameOngoing()) return;
+    strategy();
+    game.savePlayerPosition();
+  }
+
+  /**
+   * Strategy that your bot will employ while game is ongoing.
+   * It must make use of the game interface.
+   */
+  function strategy() {
+    console.log("player", game.getPlayer());
+    console.log("enemy", game.getEnemy());
+    console.log(game.getDots());
+  }
 })();
